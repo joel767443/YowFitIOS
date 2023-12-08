be3c1596253c5bacb21debf5e335877ebfe284d9
Yoweli Kachala <yowelikachala@gmail.com>
2023-12-08 16:24:13 +0200

    Initial Commit

---
+++ Shared/YoKachApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoKachApp.swift
+//  Shared
+//
+//  Created by macos on 2023/12/08.
+//
+
+import SwiftUI
+
+@main
+struct YoKachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
